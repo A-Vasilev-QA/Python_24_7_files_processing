8c3042794980c89e80144bc460d925f7bbcd5484
Aleksei_Vasilev <aleksei.vasilev@iii.com>
2026-02-07 20:16:54 +0200

    Test finished and working

--- test_files.py
+++ test_files.py
@@ -5,14 +5,10 @@ import csv
 
 from selene.support.shared import browser
 from pathlib import Path
-from selene.support.conditions import be, have
 from conftest import DOWNLOAD_DIR
 from selene import command
-
-def remove_ads(browser):
-    browser.driver.execute_script("""
-        document.querySelectorAll('iframe, .adsbygoogle, [id^="aswift"], [class*="overlay"], [class*="popup"]').forEach(el => el.remove());
-    """)
+from pypdf import PdfReader
+from openpyxl import load_workbook
 
 def test_files():
     filename_pdf = "file-sample_150kB.pdf"
@@ -34,3 +30,38 @@ def test_files():
     browser.element(f"a[href*='{filename_csv}']").perform(command.js.click)
     filepath = Path(DOWNLOAD_DIR) / filename_csv
     browser.wait.until(lambda: filepath.exists())
+
+    zip_path = os.path.join(DOWNLOAD_DIR, 'test_files.zip')
+
+    with zipfile.ZipFile(zip_path, 'w') as zf:
+        for file in (filename_pdf, filename_xlsx, filename_csv):
+            add_file = os.path.join(DOWNLOAD_DIR, file)
+            zf.write(add_file, os.path.basename(add_file))
+
+    for file in (filename_pdf, filename_xlsx, filename_csv):
+        os.remove(os.path.join(DOWNLOAD_DIR, file))
+
+    with zipfile.ZipFile(zip_path) as zip_file:
+        with zip_file.open(filename_pdf) as pdf_file:
+            reader = PdfReader(pdf_file)
+            number_of_pages = len(reader.pages)
+            first_page_text = reader.pages[0].extract_text()
+            assert number_of_pages == 4
+            assert first_page_text.startswith("Lorem ipsum")
+
+    with zipfile.ZipFile(zip_path) as zip_file:
+        with zip_file.open(filename_csv) as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            csvreader = list(csv.reader(content.splitlines()))
+            second_row = csvreader[1]
+
+            assert second_row[1] == "Dulce"
+            assert second_row[2] == "Abril"
+
+
+    with zipfile.ZipFile(zip_path) as zip_file:
+        with zip_file.open(filename_xlsx) as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+            assert sheet.cell(row=2, column=2).value == "Dulce"
+            assert sheet.cell(row=2, column=3).value == "Abril"
